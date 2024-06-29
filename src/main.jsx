cbb2a24bf9cba50224ec8ce6ce6dd95266c70556
serhat koç <kocserhat671@gmail.com>
2024-06-29 11:41:29 +0300

    mui eklendi ve düzenlemeler yapıldı

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
+import { createTheme, ThemeProvider } from '@mui/material'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={createTheme()}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
 )
