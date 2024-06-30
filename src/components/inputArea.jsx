4d4f5130e12f59fba143d2f97f3456fbf23d7ac2
serhat koç <kocserhat671@gmail.com>
2024-06-30 16:02:25 +0300

    psotlar supabaseden alındı

--- src/components/inputArea.jsx
+++ src/components/inputArea.jsx
@@ -6,6 +6,8 @@ export const InputArea = () => {
     const [postContent, setPostContent] = useState("lorem ipsum dolor sit amet consectetur adipiscing elit curabitur euismod nunc nec nisi fermentum, nec tincidunt odio tincidunt cras ");
 
     return (
+        <Stack>
+
         <Stack direction="row" gap={1} alignItems="flex-start">
             <Typography variant="h5" component="h2">
                 {user}
@@ -15,10 +17,13 @@ export const InputArea = () => {
             </Typography>
           
             <Stack flexDirection={"column"}>
-            <Typography variant="body1" component="span" textAlign="start"  sx={{wordBreak:"break-all"}}>
-                Yorum ekle...
-            </Typography>
+                
+
             </Stack>
+        </Stack>
+                    <Typography variant="body1" component="span" textAlign="start"  sx={{wordBreak:"break-all"}}>
+                    Yorum ekle...
+                </Typography>
         </Stack>
     )
 }
--- src/components/instagramWrapper.jsx
+++ src/components/instagramWrapper.jsx
@@ -1,16 +1,28 @@
 import { Stack } from "@mui/material";
 import { ProfileCard } from "./profileCard";
 import { Headers } from "./headers";
+import { useEffect } from "react";
+import { useState } from "react";
+import { supabase } from "../supabase/supabase.js";
 
 export const InstagramWrapper = () => {
+const [user, setUser] = useState([]);
+
+useEffect(() => {
+    getPost();
+}, []);
+
+    async function getPost() {
+        const {data } = await supabase.from('user').select('*');
+        setUser(data);
+    }
     return (
-        <Stack height="100%" gap={3}>
+        <Stack height={"100%"} gap={3} >
             <Headers />
-        {[...Array(30).keys()].map(element => {
-            return (
-                <ProfileCard key={element} id={element} />
-            )
-        })}
+            {user?.map(user => (
+                 <ProfileCard key={user.id} id={user.id} post={user} />
+            )    
+            )}
         </Stack>
     )
 }
